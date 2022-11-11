fa1700a0210bfa16d75a45a6eec0eac50e187c34
yanagi1115 <114564042+yanagi1115@users.noreply.github.com>
2022-11-11 11:15:02 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-let emptychart = [];
+'use strict'
+// 1行目に記載している 'use strict' は削除しないでください
 
+let emptychart = [];
 function makeCorrectChart(){     
-    let nop = document.getElementById("inputtext").value;
+    let nop = document.getElementById("inputnumber").value;
     function makeInitialChart(nop){
       //全て"x"がnop並んだオブジェクト
       function makeInitialObject(nop){
@@ -23,68 +25,57 @@ function makeCorrectChart(){
     for ( let i = 0 ; i <= nop - 1 ; i ++){
       chart[i][i] = "-";
     }
-    // output.innerText = chart; //[object Object],[object Object]…になってしまう
-    // console.log(chart);
-    // emptychart = chart;
+    emptychart = chart;
+    return emptychart;
 }
 
-function fixnop(){
-  let nop = document.getElementById("inputtext").value;
-  output.innerText = nop;
-}
-
-const output = document.getElementById("output");
 const target = document.getElementById("emptychartbutton");
-target.addEventListener('click', fixnop);
-// console.log(nop);
-console.log(output.innerText);
+target.addEventListener('click', makeCorrectChart);
 
-makeCorrectChart(output.innerText);
+const target2 = document.getElementById("savebutton");
+target2.addEventListener('click', saveInitialChart);
 
-// ココから下、一旦保留
-
-// const people = document.getElementById("inputtext").value;
-// console.log(people);
-// const chart = makeCorrectChart(people);
-// const initialchart = JSON.parse(JSON.stringify(chart));
-
-// let result = [];
-// result.push(initialchart);
-
-// function changeChart2(array){
-  
-//   newchart[array[0]][array[1]] = "o";
-//   newchart[array[1]][array[0]] = "o";
-//   return newchart;
-// }
-// const newchart = JSON.parse(JSON.stringify(initialchart));
-// function changeChartDaily2(array){
-//   for (let i = 0 ; i < array.length ; i++ ){
-//   changeChart2(array[i]);
-//   }
-//   result.push(JSON.parse(JSON.stringify(newchart)));
-// }
+let result = [];
+let initialchart = [];
+let newchart = [];
+function saveInitialChart(){
+  initialchart = JSON.parse(JSON.stringify(emptychart));
+  result.push(initialchart);
+  newchart = JSON.parse(JSON.stringify(initialchart));
+}
 
 
+// ココから下、保留
 
-// console.log("Initial chart: ", result[0]);
 
-// const day1 = [[0, 1], [2, 3], [4, 5]]
-// changeChartDaily2(day1);
-// console.log("After Day1 : ", result[1]);
+const target3 = document.getElementById("changebutton");
+target3.addEventListener('click', changeChartDaily);
 
-// const day2 = [[0, 2], [1, 4], [3, 5]];
-// changeChartDaily2(day2);
-// console.log("After Day2 : ", result[2]);
+function changeChartDaily(){
+  let num1 = document.getElementById("inputchange1").value;
+  let num2 = document.getElementById("inputchange2").value;
+    newchart[num1][num2] = "o";
+    newchart[num2][num1] = "o";
+  // result.push(JSON.parse(JSON.stringify(newchart)));
+  // return result;
+}
 
-// const day3 = [[0, 3], [1, 5], [2, 4]];
-// changeChartDaily2(day3);
-// console.log("After Day3 : ", result[3]);
+const target4 = document.getElementById("clearbutton");
+target4.addEventListener('click', clearTextbox);
 
-// const day4 = [[0, 4], [1, 3], [2, 5]];
-// changeChartDaily2(day4);
-// console.log("After Day4 : ", result[4]);
+function clearTextbox(){
+  document.getElementById("inputchange1").value = "";
+  document.getElementById("inputchange2").value = "";
+}
 
-// const day5 = [[0, 5], [1, 2], [3, 4]];
-// changeChartDaily2(day5);
-// console.log("After Day5 : ", result[5]);
+
+const target5 = document.getElementById("finishbutton");
+target5.addEventListener('click', outputChart);
+
+function outputChart(){
+  console.log(newchart);
+  console.log(result);
+  result.push(JSON.parse(JSON.stringify(newchart)));
+  // return result;
+}
+
